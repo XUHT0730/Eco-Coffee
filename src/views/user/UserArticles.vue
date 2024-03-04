a5485c7910954e1ef40620fe9b82b8e4e0541774
XUHT0730 <roy1108730@gmail.com>
2024-03-04 16:03:01 +0800

    update

--- src/views/user/UserArticles.vue
+++ src/views/user/UserArticles.vue
@@ -1,5 +1,7 @@
 <template>
+  <div class="container">
     <div>所有文章頁面</div>
+  </div>
 </template>
 <script>
 export default {
--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>使用者首頁</div>
+  <div class="container">
+    <h1>使用者首頁</h1>
+  </div>
 </template>
 <script>
 export default {
