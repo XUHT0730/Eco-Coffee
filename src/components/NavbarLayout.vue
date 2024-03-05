9a546f7d75c00daa5eb5953b5147999dda3b8546
XUHT0730 <roy1108730@gmail.com>
2024-03-05 11:29:56 +0800

    更新

--- src/components/NavbarLayout.vue
+++ src/components/NavbarLayout.vue
@@ -1,50 +1,49 @@
 <template>
   <ToastMessages />
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-      <div class="container">
-        <!--LOGO-->
-        <RouterLink to="/" class="navbar-brand">
-            <i class="bi bi-cup-hot">Eco-Coffee</i>
-        </RouterLink>
-        <!-- BUTTON -->
-        <button class="navbar-toggler"  data-bs-toggle="collapse"
-          @click="openCollapse" data-bs-target="#lokiNavbar">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <!--主角 MENU-->
-        <div id="lokiNavbar" ref="collapse" class="collapse navbar-collapse">
-          <div class="navbar-nav me-auto mb-2 mb-lg-0">
-              <RouterLink to="/products"
-              class="nav-item nav-link">產品列表</RouterLink>
-              <RouterLink to="/article"
-              class="nav-item nav-link">Blog</RouterLink>
-              <router-link to="/articles"
-              class="nav-item nav-link">所有推文頁面</router-link>
+      <nav class="navbar container navbar-expand-lg navbar-dark bg-primary sticky-top">
+        <div class="container-fluid">
+          <!--LOGO-->
+          <RouterLink to="/" class="navbar-brand">
+              <i class="bi bi-cup-hot">Eco-Coffee</i>
+          </RouterLink>
+          <!-- BUTTON -->
+          <button class="navbar-toggler"  data-bs-toggle="collapse"
+            @click="openCollapse" data-bs-target="#lokiNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <!--主角 MENU-->
+          <div id="lokiNavbar" ref="collapse" class="collapse navbar-collapse">
+            <div class="navbar-nav me-auto mb-2 mb-lg-0">
+                <RouterLink to="/products"
+                class="nav-item nav-link">產品列表</RouterLink>
+                <RouterLink to="/article"
+                class="nav-item nav-link">Blog</RouterLink>
+                <router-link to="/articles"
+                class="nav-item nav-link">所有推文頁面</router-link>
+            </div>
+            <div class="nav navbar-nav d-flex me-2">
+              <RouterLink to="/cart" class="btn btn-outline-primary
+              nav-item nav-link position-relative">
+              <i class="bi bi-cart"></i>
+              <span class="position-absolute top-0 start-100
+              translate-middle badge rounded-pill bg-danger">
+              {{ this.carts?.length }}
+              <span class="visually-hidden">unread messages</span>
+              </span>
+              </RouterLink>
+              <router-link to="/login" class="nav-item nav-link">
+              <i class="bi bi-person-circle"></i>
+              </router-link>
+            </div>
+            <!-- 因為在 navbar-collapse 內，所以一同響應隱藏 -->
+            <!-- <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form> -->
           </div>
-          <div class="nav navbar-nav d-flex me-2">
-            <RouterLink to="/cart" class="btn btn-outline-primary
-            nav-item nav-link position-relative">
-            <i class="bi bi-cart"></i>
-            <span class="position-absolute top-0 start-100
-            translate-middle badge rounded-pill bg-danger">
-            {{ this.carts?.length }}
-            <span class="visually-hidden">unread messages</span>
-            </span>
-            </RouterLink>
-            <router-link to="/login" class="nav-item nav-link">
-            <i class="bi bi-person-circle"></i>
-            </router-link>
-          </div>
-          <!-- 因為在 navbar-collapse 內，所以一同響應隱藏 -->
-          <!-- <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form> -->
+
         </div>
-      </div>
      </nav>
-    </div>
 </template>
 
 <style lang="scss">
--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -1,8 +1,57 @@
 <template>
-  <div class="container">
-    <h1>使用者首頁</h1>
+  <header class="container header"
+   style="height: 504px; background-image: url(https://storage.googleapis.com/vue-course-api.appspot.com/hedy-api-path/1709543433230.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=RKdXij%2BJKl7scyyCrzbEoL7DKU%2BhtTvd%2B19Qhee9AUDq%2FoXrgyD8lS%2BZCheCWRKgOfScfomYaSL3rgCwbQeThOe2rtiKYmypjiZnqy1hZF0ldnixATNZZaCldVhwKytNUTZHjWoP58IeUI9FLiRG6PMhzknkCEzSP4OdmF54GOEvdYBkQp3D1QhPENbhBB4bjd9WbxuGjJHRuDY5B4pItsX4ZbMPpLQQeOpGiO%2F2AjrnOIW2giK%2BDc4eD4SAZowcPBNK8YJCo79Z9KqZQ6lRS%2FInyi%2BNkpH3UnYfYzqFTN6PEHNva892jSwMMYMtKCu5cC7ReW%2FfkBTEY%2B9GR2KF7g%3D%3D);">
+   <div class="d-flex align-items-center h-100 position-relative" style="z-index: 1;">
+    <div class="header-text">
+      <h1>愛咖啡更支持咖啡</h1>
+      <button class="btn btn-primary btn-lg" type="button">了解公平貿易</button>
+    </div>
   </div>
+  </header>
+  <section class="container mt-6">
+    <h2 class="text-center">選購公平貿易咖啡商品</h2>
+    <div class="row justify-content-center">
+      <div class="col-md-5">
+      </div>
+      <div class="col-md-5 d-flex align-items-center">
+        <div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+
+  </section>
 </template>
+
+<style lang="scss">
+  .header {
+    height: 504px;
+    background-position: center center;
+    background-size: cover;
+    position: relative;
+  }
+  .header::after {
+  position: absolute;
+  content: "";
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: black;
+  opacity: 0.3;
+}
+.header-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+</style>
+
 <script>
 export default {
   data() {
