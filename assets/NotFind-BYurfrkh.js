4929289f66f479ae919e55d4ae197fdaa93ecd2b
XUHT0730 <roy1108730@gmail.com>
2024-03-07 19:43:47 +0800

    Updates

---
+++ assets/NotFind-BYurfrkh.js
@@ -0,0 +1,1 @@
+import{_ as n,r as a,o as c,c as s,b as o,f as e,a as r,e as _}from"./index-DWtJNFht.js";const l={},d=o("h1",null,"Not Found",-1);function u(i,f){const t=a("RouterLink");return c(),s("div",null,[d,o("p",null,[e("笑死，找不到頁面，回 "),r(t,{to:"/"},{default:_(()=>[e("首頁")]),_:1}),e(" 把 ")])])}const m=n(l,[["render",u]]);export{m as default};
